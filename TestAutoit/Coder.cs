c3e554f306cc5cbf5728a0cec6e475ceb89b16f9
vicmatmar <victormartin@hotmail.com>
2015-11-17 13:23:21 -0600

    pixoto

---
+++ TestAutoit/Coder.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Threading;
+using System.Diagnostics;
+
+using AutoIt;
+
+namespace TestAutoit
+{
+    class Coder
+    {
+        int _cred = 0xFF0000;
+        int _cgreen = 0x008000;
+        Point _point_State_color;
+        TimeSpan _timeout;
+
+        public Coder(TimeSpan timeout)
+        {
+            _timeout = timeout;
+        }
+
+        IntPtr getWin(string win_desc)
+        {
+            string msg;
+
+            if (AutoItX.WinExists(win_desc) != 1)
+            {
+                msg = string.Format("Unable to find window \"{0}\"", win_desc);
+                throw new Exception(msg);
+            }
+
+            IntPtr hwnd = AutoItX.WinGetHandle(win_desc);
+            if (AutoItX.WinExists(win_desc) != 1)
+            {
+                msg = string.Format("Unable to get handle for window \"{0}\"", win_desc);
+                throw new Exception(msg);
+            }
+
+            return hwnd;
+        }
+
+        IntPtr activate_win(string win_desc)
+        {
+            IntPtr hwnd = getWin(win_desc);
+
+            string msg;
+            int n = 0;
+            while (AutoItX.WinActive(hwnd) != 1)
+            {
+                AutoItX.WinActivate(hwnd);
+                Thread.Sleep(250);
+                if (n++ > 10)
+                {
+                    msg = string.Format("Unable to activate window \"{0}\"", win_desc);
+                    throw new Exception(msg);
+                }
+            }
+
+            return hwnd;
+        }
+
+        public void Code(CancellationToken cancel)
+        {
+            if (cancel.IsCancellationRequested)
+                return;
+
+            string win_desc = "[REGEXPTITLE:.*Pixoto - Google Chrome.*]";
+            IntPtr hwnd = activate_win(win_desc);
+
+            Rectangle pos = AutoItX.WinGetPos(hwnd);
+            Rectangle rec = AutoItX.WinGetClientSize(hwnd);
+
+            int left = pos.X + rec.Width / 2 - 200;
+            int right = pos.X + rec.Width / 2 + 200;
+
+            Random random = new Random();
+            int mod = random.Next(3, 5);
+
+            int right_or_left = right;
+            for (int i = 0; i < 500; i++)
+            {
+                AutoItX.MouseMove(right_or_left, pos.Y + 500, 2);
+                AutoItX.MouseClick();
+                Thread.Sleep(2000);
+
+                if (i % mod == 0)
+                {
+                    mod = random.Next(3, 5);
+
+                    if (right_or_left == right)
+                        right_or_left = left;
+                    else
+                        right_or_left = right;
+                }
+            }
+
+        }
+    }
+}
---
+++ TestAutoit/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Windows;
+using System.Diagnostics;
+
+using System.Runtime.InteropServices;
+using AutoIt;
+
+using System.Windows.Automation;
+using System.Text.RegularExpressions;
+
+namespace TestAutoit
+{
+    public partial class Form1 : Form
+    {
+        Point _point_State_color = new Point();
+        string _coding_error_msg;
+        CancellationTokenSource _tokenSrcCancel = new CancellationTokenSource();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //button4_Click(this, EventArgs.Empty);
+        }
+
+        void coding_done_handler(Task task)
+        {
+            bool canceled = task.IsCanceled;
+            var exception = task.Exception;
+        }
+
+        void coding_exception_handler(Task task)
+        {
+            var exception = task.Exception;
+            string errmsg = exception.InnerException.Message;
+            _coding_error_msg = errmsg;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Coder coder = new Coder(new TimeSpan(0,2,0));
+            CancellationToken token = _tokenSrcCancel.Token;
+            Task task = new Task( () => coder.Code(token), token);
+            task.ContinueWith(coding_exception_handler, TaskContinuationOptions.OnlyOnFaulted);
+            task.ContinueWith(coding_done_handler, TaskContinuationOptions.OnlyOnRanToCompletion);
+            task.Start();
+        }
+
+        void MouseHook_MouseAction(object sender, EventArgs e)
+        {
+            MouseHook.Stop();
+
+            MouseHook.POINT p = new MouseHook.POINT();
+            MouseHook.GetCursorPos(out p);
+
+            label1.Text = string.Format("X = {0}, Y = {1}", p.x, p.y);
+
+            _point_State_color = new Point(p.x, p.y);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            AutomationElement desktop = AutomationElement.RootElement;
+
+            PropertyCondition condition = new PropertyCondition(AutomationElement.ControlTypeProperty, System.Windows.Automation.ControlType.Window);
+            AutomationElementCollection list = desktop.FindAll(TreeScope.Children, condition);
+            AutomationElement win = null;
+            foreach (AutomationElement item in list)
+            {
+                object name = item.GetCurrentPropertyValue(AutomationElement.NameProperty, false);
+                string title = (string)name;
+                Match match = Regex.Match(title, "Pixoto - Google Chrome");
+                if (match.Success)
+                {
+                    win = item;
+                    break;
+                }
+            }
+
+            condition = new PropertyCondition(AutomationElement.ClassNameProperty, "Chrome_RenderWidgetHostHWND");
+            AutomationElement wndvote = win.FindFirst(TreeScope.Children, condition);
+
+            //wndvote.GetClickablePoint();
+
+            InvokePattern click = wndvote.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+            click.Invoke();
+
+
+        }
+
+        private void buttonGetXY_Click(object sender, EventArgs e)
+        {
+            MouseHook.Start();
+            MouseHook.MouseAction += MouseHook_MouseAction;
+
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            _tokenSrcCancel.Cancel();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
